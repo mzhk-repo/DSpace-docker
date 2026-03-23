a258e44d398d33de8c8eaa118c151550c1fd5ee3
pinokew <cestlawie235@gmail.com>
2026-03-23 15:23:28 +0200

    refactoring v2

--- scripts/deploy-orchestrator.sh
+++ scripts/deploy-orchestrator.sh
@@ -9,7 +9,6 @@ touch dspace/config/local.cfg
 [[ -f "./scripts/verify-env.sh" ]] && bash ./scripts/verify-env.sh --ci-mock
 [[ -f "./scripts/patch-local.cfg.sh" ]] && bash ./scripts/patch-local.cfg.sh
 [[ -f "./scripts/patch-config.yml.sh" ]] && bash ./scripts/patch-config.yml.sh
-[[ -f "./scripts/init-volumes.sh" ]] && bash ./scripts/init-volumes.sh .env
 [[ -f "./scripts/smoke-test.sh" ]] && bash ./scripts/smoke-test.sh
 
 echo "Orchestration script completed"
